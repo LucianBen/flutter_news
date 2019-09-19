6f529a14a64133db3a5f4dcce48dac00d8db0942
LucianBenn <1346742014@qq.com>
2019-09-19 17:45:11 +0800

    新闻首页 科技页面绘制

--- lib/pages/page_news/technology/news_technology.dart
+++ lib/pages/page_news/technology/news_technology.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class NewsTechnology extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text(),
+    );
   }
 }
